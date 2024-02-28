eb7d997655a3a9794c317a7f70e294290d24e004
Whitenoizzz <lovey242001@gmail.com>
2024-02-28 22:05:50 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Skills from './components/Skills'
 import NavbarTemp from './components/NavbarTemp'
+import About from './components/About'
+import Projects from './components/Projects'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <Skills></Skills>
+      <About></About>
+      <Projects></Projects>
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import aboutSvg from '../assets/about.svg'
+import SectionTitle from './SectionTitle'
+const About = () => {
+  return (
+    <section className="bg-white py-20" id="about">
+      <div className="align-component grid md:grid-cols-2 items-center gap-16">
+        <img src={aboutSvg} className="w-full h-64" />
+        <article>
+          <SectionTitle text="code and tea" />
+          <p className="text-slate-600 mt-8 leading-loose md:pr-4">
+            Guten Tag! 👋 I'm , a student and aspiring software developer
+            currently based in Germany, with roots tracing back to the colorful
+            landscapes of India. Embracing the fusion of cultures, I find
+            inspiration in the rich flavors of chai, the rhythmic beats of
+            music, and the endless possibilities of coding. Though still
+            navigating the early stages of my journey, I'm passionate about
+            leveraging technology to craft meaningful experiences. When I'm not
+            buried in textbooks or lines of code, you can often find me
+            exploring new cafes in search of the perfect cup of tea or unwinding
+            to the tunes of my favorite playlist.
+          </p>
+        </article>
+      </div>
+    </section>
+  )
+}
+export default About
